b3f621a39e225a65cb56e5f709acb59f4f51f420
Owen Kealey <owen@owenkealey.io>
2018-07-13 00:53:39 -0400

    basically finished it lmao

--- js/newtab.js
+++ js/newtab.js
@@ -1,9 +1,11 @@
+// Copyright 2018 Grand Street Technologies. All Rights Reserved.
 function getStudentInfo(){
-    var appUrl = "URL";
+    var appUrl = "https://script.google.com/macros/s/AKfycbxAS9LZunbUUhVYlQmGVk3UgIud9-YEJnvm6lYJNPALUcCJ4ME/exec";
     var loggedInUser
     chrome.extension.sendMessage({}, function(response) {
         loggedInUser = response.email
     });
+    console.log(loggedInUser)
     var studentInfo;
     $.post(appUrl, loggedInUser, function(data) {
         localStorage.setItem("studentInfo", data);
@@ -13,12 +15,12 @@ function getStudentInfo(){
 
 
 function renderInfo(){
-    var studentInfo = localStorage.getItem("studentId");
+    var studentInfo = localStorage.getItem("studentInfo");
     if(studentInfo == null){
         var studentInfo = getStudentInfo();
     }
     var studentInfo = JSON.parse(studentInfo);
-    var name = studentInfo["name"].split(" ")[0];
+    var name = studentInfo["name"];
     var nameElement = document.getElementById("name");
     var powerSchoolElement = document.getElementById("powerschool");
     var scheduleElement = document.getElementById("schedule");
@@ -27,16 +29,16 @@ function renderInfo(){
     var h = d.getHours();
     var formattedDate = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
     if(h < 12){
-        nameElement.innerHTML = "Good morning " + name;
+        nameElement.innerHTML = "Good morning, " + name;
     }
     else if(h < 17){
-        nameElement.innerHTML = "Good afternoon " + name;
+        nameElement.innerHTML = "Good afternoon, " + name;
     }
     else{
-        nameElement.innerHTML = "Good evening " + name;
+        nameElement.innerHTML = "Good evening, " + name;
     }
     if(studentInfo["email"].contains("students")){
-        powerSchoolElement.setAttribute("href", "powerschool.spprep.org");
+        powerSchoolElement.setAttribute("href", "https://powerschool.spprep.org");
     }
     else{
         powerSchoolElement.setAttribute("href", "https://spprep.powerschool.com/teachers/pw.html");
@@ -51,8 +53,9 @@ function time() {
     var s = d.getSeconds();
     var m = d.getMinutes();
     var h = d.getHours();
-    var span = document.getElementById('time');
-    span.textContent = h + ":" + m + ":" + s;
+    var div = document.getElementById('time');
+    div.textContent = h + ":" + m + ":" + s;
     }
+time()
 setInterval(time, 1000);
 renderInfo()
