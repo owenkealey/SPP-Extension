3e877d35f383d720ef592ccca9e33d694a142aa8
Owen Kealey <owen@owenkealey.io>
2018-07-16 10:39:18 -0400

    added final touches

--- js/newtab.js
+++ js/newtab.js
@@ -1,6 +1,5 @@
 // Copyright 2018 Grand Street Technologies. All Rights Reserved.
 function setInfo(data){
-    console.log("GOT DATA" + data);
     localStorage.setItem("studentInfo", data);
 }
 
@@ -9,8 +8,6 @@ function setStudentInfo(){
     var appUrl = "https://script.google.com/macros/s/AKfycbxAS9LZunbUUhVYlQmGVk3UgIud9-YEJnvm6lYJNPALUcCJ4ME/exec";
     chrome.identity.getProfileUserInfo(function(info) { 
         email = info.email; 
-        console.log("received " + email);
-        console.log("submitting ajax")
         $.ajax({
             type: "POST",
             url: appUrl, 
@@ -22,7 +19,6 @@ function setStudentInfo(){
 
 function completeRender(){
     var studentInfo = localStorage.getItem("studentInfo");
-    console.log("FINALLY GOT" + studentInfo)
     studentInfo = JSON.parse(studentInfo);
     var name = studentInfo["name"];
     var nameElement = document.getElementById("name");
@@ -31,7 +27,6 @@ function completeRender(){
     var announcementsElement = document.getElementById("announcements");
     var d = new Date();
     var h = d.getHours();
-    var formattedDate = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
     if(h < 12){
         nameElement.innerHTML = "Good morning, " + name;
     }
@@ -47,6 +42,16 @@ function completeRender(){
     else{
         powerSchoolElement.setAttribute("href", "https://spprep.powerschool.com/teachers/pw.html");
     }
+    var year = d.getFullYear();
+    var month = d.getMonth() + 1;
+    var day = d.getDate();
+    if(month < 10){
+        month = "0" + month;
+    }
+    else if(day < 10){
+        day = "0" + day;
+    }
+    var formattedDate = year + "-" + month + "-" + day;
     scheduleElement.setAttribute("href", "http://intranet.spprep.org/images/pdf/Student_Schedules/Current_Student_Schedules/" + studentInfo["id"] + ".pdf");
     announcementsElement.setAttribute("href", "http://intranet.spprep.org/calendar/announcements/" + formattedDate + ".html");
 }
@@ -66,16 +71,44 @@ function renderInfo(){
 }
 
 
+function refresh(){
+    localStorage.removeItem("studentInfo");
+    location.reload();
+}
+
+
+function handleRefresh(){
+    document.getElementById("refresh").addEventListener("click", refresh);
+}
+
+
 function time() {
     var d = new Date();
     var s = d.getSeconds();
     var m = d.getMinutes();
     var h = d.getHours();
+    if(s < 10){
+        s = "0" + s;
+    }
+    if(m < 10){
+        m = "0" + m;
+    }
+    if(h < 10){
+        h = "0" + h;
+    }
+    if(h <= 12){
+        s = s + " am";
+    }
+    else{
+        h = h - 12;
+        s = s + " pm";
+    }
     var div = document.getElementById('time');
     div.textContent = h + ":" + m + ":" + s;
 }
 
 
-time()
-renderInfo()
+time();
+renderInfo();
+handleRefresh();
 setInterval(time, 1000);
