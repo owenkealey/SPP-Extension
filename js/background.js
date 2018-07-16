231d7205db86ecc3b682dfe5d1f65b91243ab40d
Owen Kealey <owen@owenkealey.io>
2018-07-16 10:47:45 -0400

    Delete background.js

--- js/background.js
+++
@@ -1,4 +0,0 @@
-chrome.identity.getProfileUserInfo(function(info) { email = info.email; });
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-    sendResponse(email)
-});
